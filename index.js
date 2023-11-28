c9c3954daa857231041746eaccb64b2d94795bf2
Philipp Bartels <pb@handmade-systems.de>
2023-11-28 08:43:35 +0100

    try split configs

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict';
 const configNext = require('./lib/configs/next');
+const configBase = require('./lib/configs/base');
+const configReact = require('./lib/configs/react');
 
 module.exports = {
     // rules: allRules,
     configs: {
         next: configNext,
+        react: configReact,
+        base: configBase,
     },
 };
