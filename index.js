f8b524459cea95cfbade4dd85fb900a2d78c3ca4
phibar <pb@handmade-systems.de>
2023-11-28 12:30:02 +0100

    feat:Added additional configs (#43)
    * updated deps
    
    * updated next
    
    * added playright, storybook, tailwind configs

--- index.js
+++ index.js
@@ -2,12 +2,17 @@
 const configNext = require('./lib/configs/next');
 const configBase = require('./lib/configs/base');
 const configReact = require('./lib/configs/react');
+const configStorybook = require('./lib/configs/storybook');
+const configTailwind = require('./lib/configs/tailwind');
+const configPlaywright = require('./lib/configs/playwright');
 
 module.exports = {
-    // rules: allRules,
     configs: {
         next: configNext,
         react: configReact,
         base: configBase,
+        storybook: configStorybook,
+        tailwind: configTailwind,
+        playwright: configPlaywright,
     },
 };
